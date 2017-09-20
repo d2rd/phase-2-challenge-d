05e40a11f4c0a5a47c54fb649f3d62bb8dbae185
Glenn-David Daniel <d2rd@outlook.com>
2017-09-20 10:25:53 -0700

    WIP function setup

--- part-1/functions.js
+++ part-1/functions.js
@@ -23,18 +23,28 @@ function snippet(string, maxlength) {
 }
 
 //Number of properties
-// Write a function numProps(obj) that returns the number of properties an object has. Ignore symbolic properties and count only the "own properties" (not inherited) of the object.
+// Write a function numProps(obj) that returns the num of properties an object has. Ignore symbolic properties and count only the "own properties" (not inherited) of the object.
 
 function numProps(obj){
 
 }
 
-// Filter between
+//filterBetween
 // Write a function filterBetween(array, min, max) that takes an array of numbers, a min value, and a max value. It returns a new array containing only the elements that are greater than or equal to min and less than or equal to max.
+// Example:
+  // let arr = [5, 10, 15, 20, 25, 30, 35]
+  // filterBetween(arr, 15, 34) // => [15, 20, 25, 30]
+  // filterBetween(arr, 22, 100) // => [25, 30, 35]
 
-function filterBetween(array, min, max){
-  
+var minMaxNum = process.argv[2]+", "+process.argv[3]; //min/max arguments entered as string ("min, max"), are the 3rd and 4th array items in process.argv. 
+console.log(minMaxNum); // check to confirm arguments passed through.
+let numArray = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+// console.log(numArray); // check to confirm array is populated;
+function filterBetween(numArray, minNum, maxNum) {
+
 }
 
 
 
+
+module.exports = {weekday, filterBetween, snippet, numProps}
--- part-1/tests.js
+++ part-1/tests.js
@@ -1,12 +1,12 @@
 var {expect} = require ('chai')
-var { weekday, snippet, numProps, filterBetween }  = require('./functions')
+var {weekday, filterBetween, snippet, numProps}  = require('./functions')
 
 
 describe('weekday()', function () {
   it('should return a function', function() {
     expect(weekday).to.be.a('function')
   })
-  it('should return the weekday of a given date', function(){
+  it('should return the day of the week for a given date', function(){
 
     let targetDate = new Date(2017, 5, 15)
 
@@ -19,64 +19,20 @@ describe('weekday()', function () {
 })
 
 
-
-
-
-describe.only('capitalizeFourth()', function(){
+describe('filterBetween()', function(){
 
   it('should return a function', function(){
-    expect(capitalizeFourth).to.be.a('function')
+    expect(filterBetween).to.be.a('function')
   })
 
-  it('should return a string where every 4th character is converted to uppercase and the rest, lowercase', function() {
-
-  })
-  let string = [3,4,5,6]
-
-  it( "should throw an error if typeof string !== 'string'"), function (){
-var myFunction = function() {return capitalizeFourth(89)}
-    expect(myFunction).to.throw(Error('invalid input'))
-  }
-
-
-describe('getValues()', function () {
-  it('should return a function', function() {
-    expect(getValues).to.be.a('function')
-  })
-
-  it('should return the values of the properties in an object', function() {
-
-  let person = {
-  name: 'Dominique',
-  age: 30,
-  phone: '555-555-5555'
-}
-
-    expect(getValues(person)).to.eql(['Dominique', 30, '555-555-5555'])
-  })
-  it('should throw an error if an object is not passed in', function(){
-   expect(getValues).to.throw(Error)
-  })
-})
-
-//
-//
-//
-//
-describe('filterAround()', function(){
-
-  it('should return a function', function(){
-    expect(filterAround).to.be.a('function')
-  })
-
-  it('returns a new array containing only the elements from the source array that come before lower alphabetically and after upper, function()', function(){
+  it('returns a new array containing only the values between the values of min and max arguments, function()', function(){
     let animals = ['dog', 'cat', 'zebra', 'ape', 'lion', 'cow']
-     expect(filterAround(animals, 'chimp', 'lobster')).to.eql(['cat', 'zebra', 'ape'])
+     expect(filterBetween(animals, 'chimp', 'lobster')).to.eql(['cat', 'zebra', 'ape'])
    })
 
   let animals = ("24", "dog")
 
   it('throws an error if an object is not passed through', function(){
-     expect(filterAround).to.throw(Error)
+     expect(filterBetween).to.throw(Error)
    })
  })
